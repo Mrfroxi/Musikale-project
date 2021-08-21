702aa1fb1542f368ebece0a315f5be8824c964d3
Mrfroxi <froxi340@mail.ru>
2021-08-21 14:10:45 +0300

    Fix bag with track

--- server/models/models.js
+++ server/models/models.js
@@ -13,13 +13,6 @@ const PlayList = sequelize.define('playList', {
   name: { type: DataTypes.STRING },
 });
 
-const Track = sequelize.define('track', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING, unique: true, allowNull: false },
-  artist: { type: DataTypes.STRING, unique: false, allowNull: false },
-  pictureData: { type: DataTypes.STRING },
-});
-
 const Track = sequelize.define('track' ,{
   id :{type:DataTypes.INTEGER , primaryKey :true  , autoIncrement : true},
   name :{type:DataTypes.STRING ,unique:true , allowNull :false },
